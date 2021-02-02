ee1b60d8489955a37dc8f923966e175d872df856
EM-MW <76971970+EM-MW@users.noreply.github.com>
2021-02-01 18:06:02 -0600

    Adds changes to single image

--- pic2ascii.py
+++ pic2ascii.py
@@ -4,13 +4,12 @@ from tqdm import tqdm
 import math
 from tkinter.filedialog import askopenfilenames
 from tkinter import *
+from time import sleep
 chars = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'. "[::-1]
 # chars = "#Wo- "[::-1]
 charArray = list(chars)
 charLength = len(charArray)
 interval = charLength/256
-
-scaleFactor = .08
 
 oneCharWidth = 10
 oneCharHeight = 18
@@ -18,64 +17,43 @@ oneCharHeight = 18
 def getChar(inputInt):
     return charArray[math.floor(inputInt*interval)]
 
-#text_file = open("Output.txt", "w")
-y_or_n = str(input('Hi, do you want to convert many pictures? (y) or (n)?'))
-
-if y_or_n == 'y':
-    fileman = Tk()
-    fileman.wm_state('iconic')
-    file_path_list = askopenfilenames(filetypes=(("JPEG/JPG files","*.jpeg *.jpg"), ("Any file", "*")), initialdir="/", title='Select pictures.')
-    file_path_list = list(file_path_list)
-    
-    back = str(chr(92))
-    x = 0
-    for file in file_path_list:
-        
-        im = PIL.Image.open(file_path_list[x])
-        
-
-        fnt = ImageFont.truetype('C:\\Windows\\Fonts\\lucon.ttf', 15)
-
-        width, height = im.size
-        im = im.resize((int(scaleFactor*width), int(scaleFactor*height*(oneCharWidth/oneCharHeight))), PIL.Image.NEAREST)
-        width, height = im.size
-        pix = im.load()
-
-        outputImage = PIL.Image.new('RGB', (oneCharWidth * width, oneCharHeight * height), color = (0, 0, 0))
-        d = ImageDraw.Draw(outputImage)
-
-        for i in tqdm(range(height)):
-            for j in range(width):
-                r, g, b = pix[j, i]
-                h = int(r/3 + g/3 + b/3)
-                pix[j, i] = (h, h, h)
-                #text_file.write(getChar(h))
-                d.text((j*oneCharWidth, i*oneCharHeight), getChar(h), font = fnt, fill = (r, g, b))
-        
-        r = file_path_list[x].replace('/', back)
-        x += 1
-        if x < 10:
-            outputImage.save(r.replace('.jpg', '.png'))
-        elif x < 100:
-            outputImage.save(r.replace('.jpg', '.png'))
-        elif x < 1000:
-            outputImage.save(r.replace('.jpg', '.png'))
-        else:
-            outputImage.save(r.replace('.jpg', '.png'))
-        
-
+sf = input('Hello Enter Scale Factor or \'d\' for the default scale factor')
+if 'd' in sf:
+    scaleFactor = .05
 else:
-    filename = int(input('Enter the filename of the picture. This value must be a number. Make sure to have your pic in .jpg format.'))
-    im = Image.open(filename)
+    try:
+        int(sf)
+    except:
+        print('this does not work')
+        sleep(.2)
+        print('exiting')
+        sleep(.5)
+        exit()
+    else:
+        sf = scaleFactor
+        sf = _
+print('select image or image sequance.')
+sleep(.5)
+fileman = Tk()
+fileman.wm_state('iconic')
+file_path_list = askopenfilenames(filetypes=(("JPEG/JPG files","*.jpeg *.jpg"), ("Any file", "*")), initialdir="/", title='Select pictures.')
+file_path_list = list(file_path_list)
+
+back = str(chr(92))
+x = 0
+for file in file_path_list:
+        
+    im = PIL.Image.open(file_path_list[x])
+        
 
     fnt = ImageFont.truetype('C:\\Windows\\Fonts\\lucon.ttf', 15)
 
     width, height = im.size
-    im = im.resize((int(scaleFactor*width), int(scaleFactor*height*(oneCharWidth/oneCharHeight))), Image.NEAREST)
+    im = im.resize((int(scaleFactor*width), int(scaleFactor*height*(oneCharWidth/oneCharHeight))), PIL.Image.NEAREST)
     width, height = im.size
     pix = im.load()
 
-    outputImage = Image.new('RGB', (oneCharWidth * width, oneCharHeight * height), color = (0, 0, 0))
+    outputImage = PIL.Image.new('RGB', (oneCharWidth * width, oneCharHeight * height), color = (0, 0, 0))
     d = ImageDraw.Draw(outputImage)
 
     for i in tqdm(range(height)):
@@ -85,7 +63,16 @@ else:
             pix[j, i] = (h, h, h)
             #text_file.write(getChar(h))
             d.text((j*oneCharWidth, i*oneCharHeight), getChar(h), font = fnt, fill = (r, g, b))
+        
+    r = file_path_list[x].replace('/', back)
+    x += 1
+    if x < 10:
+        outputImage.save(r.replace('.jpg', '.png'))
+    elif x < 100:
+        outputImage.save(r.replace('.jpg', '.png'))
+    elif x < 1000:
+        outputImage.save(r.replace('.jpg', '.png'))
+    else:
+        outputImage.save(r.replace('.jpg', '.png'))
 
-    #text_file.write('\n')
-
-    outputImage.save(filename +'ascii')
+input('done(enter to exit)')
