07b25389e3f9cab429de5610508ec9710a5fd36b
EM-MW <76971970+EM-MW@users.noreply.github.com>
2021-02-12 15:35:21 -0800

    Update pic2ascii.py

--- pic2ascii.py
+++ pic2ascii.py
@@ -5,6 +5,8 @@ import math
 from tkinter.filedialog import askopenfilenames
 from tkinter import *
 from time import sleep
+from multiprocessing import Process
+
 chars = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'. "[::-1]
 charArray = list(chars)
 charLength = len(charArray)
@@ -13,62 +15,72 @@ interval = charLength/256
 oneCharWidth = 10
 oneCharHeight = 18
 
-def getChar(inputInt):
-    return charArray[math.floor(inputInt*interval)]
+class start:
+    def getChar(self, inputInt):
+        return charArray[math.floor(inputInt*interval)]
 
-scaleFactor = input('Hello Enter Scale Factor')
-try:
-    int(scaleFactor)
-except:
-    print('this does not work')
-    sleep(.2)
-    print('making default')
-    sleep(.5)
-    scaleFactor = 0.4
-print('select image or image sequance.')
-sleep(.5)
-print('opening window')
-sleep(.2)
-fileman = Tk()
-fileman.wm_state('iconic')
-file_path_list = askopenfilenames(filetypes=(("JPEG/JPG files","*.jpeg *.jpg"), ("Any file", "*")), initialdir="/", title='Select pictures.')
-file_path_list = list(file_path_list)
-
-#backslash
-back = str(chr(92))
-x = 0
-for file in file_path_list:
+    def main(self):
+        scaleFactor = input('Hello Enter Scale Factor')
+        try:
+            int(scaleFactor)
+        except:
+            print('this does not work')
+            sleep(.2)
+            print('making default')
+            sleep(.5)
+            scaleFactor = 0.4
+        print('select image or image sequance.')
+        sleep(.5)
+        print('opening window')
+        sleep(.2)
+        fileman = Tk()
+        fileman.wm_state('iconic')
+        file_path_list = askopenfilenames(filetypes=(("JPEG/JPG files","*.jpeg *.jpg"), ("Any file", "*")), initialdir="/", title='Select pictures.')
+        file_path_list = list(file_path_list)
         
-    im = PIL.Image.open(file_path_list[x])
-        
+        x = 0
+        for file in file_path_list:
+                
+            im = PIL.Image.open(file_path_list[x])
+                
 
-    fnt = ImageFont.truetype('C:\\Windows\\Fonts\\lucon.ttf', 15)
+            fnt = ImageFont.truetype('C:\\Windows\\Fonts\\lucon.ttf', 15)
 
-    width, height = im.size
-    im = im.resize((int(scaleFactor*width), int(scaleFactor*height*(oneCharWidth/oneCharHeight))), PIL.Image.NEAREST)
-    width, height = im.size
-    pix = im.load()
+            width, height = im.size
+            im = im.resize((int(scaleFactor*width), int(scaleFactor*height*(oneCharWidth/oneCharHeight))), PIL.Image.NEAREST)
+            width, height = im.size
+            pix = im.load()
 
-    outputImage = PIL.Image.new('RGB', (oneCharWidth * width, oneCharHeight * height), color = (64, 64, 64))
-    #outputImage = im
-    d = ImageDraw.Draw(outputImage)
+            outputImage = PIL.Image.new('RGB', (oneCharWidth * width, oneCharHeight * height), color = (64, 64, 64))
+            #outputImage = im
+            d = ImageDraw.Draw(outputImage)
 
-    for i in tqdm(range(height)):
-        for j in range(width):
-            r, g, b = pix[j, i]
-            h = int(r/3 + g/3 + b/3)
-            pix[j, i] = (h, h, h)
-            d.text((j*oneCharWidth, i*oneCharHeight), getChar(h), font = fnt, fill = (r, g, b))
-        
-    r = file_path_list[x].replace('/', back)
-    x += 1
-    if x < 10:
-        outputImage.save(r.replace('.jpg', '.png'))
-    elif x < 100:
-        outputImage.save(r.replace('.jpg', '.png'))
-    elif x < 1000:
-        outputImage.save(r.replace('.jpg', '.png'))
-    else:
-        outputImage.save(r.replace('.jpg', '.png'))
+            for i in tqdm(range(height)):
+                for j in range(width):
+                    r, g, b = pix[j, i]
+                    h = int(r/3 + g/3 + b/3)
+                    pix[j, i] = (h, h, h)
+                    d.text((j*oneCharWidth, i*oneCharHeight), getChar(h), font = fnt, fill = (r, g, b))
+                
+            r = file_path_list[x].replace('/', '\\')
+            x += 1
+            if x < 10:
+                outputImage.save(r.replace('.jpg', '.png'))
+            elif x < 100:
+                outputImage.save(r.replace('.jpg', '.png'))
+            elif x < 1000:
+                outputImage.save(r.replace('.jpg', '.png'))
+            else:
+                outputImage.save(r.replace('.jpg', '.png'))
+#for class
+p = start()
 
-input('done(enter to exit)')
+# if __name__ == '__main__':
+    # p1 = Process(target=p.main)
+    # input('yes')
+    # p1.start()
+
+    # p1.join()
+    # input('done(enter to exit)')
+
+p.main()
