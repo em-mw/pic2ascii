d380337662f2432eb97330c65ece134592b0da52
mw <76971970+em-mw@users.noreply.github.com>
2021-03-29 19:55:31 -0500

    Random things added

---
+++ dastest/New Text Document.py
@@ -0,0 +1,12 @@
+from multiprocessing import Process
+from tkinter import *
+def tar():
+    Tk()
+
+if __name__ == '__main__': 
+    p1 = Process(target=tar)
+    p2 = Process(target=tar)
+    p1.start()
+    p1.join()
+    p2.start()
+    p2.join()
