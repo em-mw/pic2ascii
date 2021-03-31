aac9722ae1510df5a83afe70f8eb669a6998796b
mw <76971970+em-mw@users.noreply.github.com>
2021-03-31 12:19:04 -0500

    Tested things

---
+++ dastest/New Text Document - Copy.py
@@ -0,0 +1,16 @@
+import sys
+from PyQt6.QtWidgets import QApplication, QWidget
+
+class ui_MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+    def closeEvent(self, event):
+        print('colsed')
+        y = 'yes'
+        if y == 'yes':
+            event.ignore()
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    MainWindow = ui_MainWindow()
+    MainWindow.show()
+    sys.exit(app.exec())
