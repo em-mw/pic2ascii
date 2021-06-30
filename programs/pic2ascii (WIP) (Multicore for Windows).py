063dc858b5d6bc9f47c630b16b2310c1e2cdc3ee
mw <01350311max247@gmail.com>
2021-06-30 18:30:33 -0500

    save point 1 for multiprocess

--- programs/pic2ascii (WIP) (Multicore for Windows).py
+++ programs/pic2ascii (WIP) (Multicore for Windows).py
@@ -2,7 +2,7 @@ import math
 from time import sleep
 from tkinter import *
 from tkinter.filedialog import askopenfilenames
-from os import getcwd
+#from os import getcwd
 import os
 import shutil
 
@@ -15,6 +15,39 @@ oneCharHeight = 18
 scaleFactor = 0.09
 properseq = int(3)
 
+def truncate_utf8_chars(filename, count, ignore_newlines=True):
+    """
+    Truncates last `count` characters of a text file encoded in UTF-8.
+    :param filename: The path to the text file to read
+    :param count: Number of UTF-8 characters to remove from the end of the file
+    :param ignore_newlines: Set to true, if the newline character at the end of the file should be ignored
+    """
+    with open(filename, 'rb+') as f:
+        last_char = None
+
+        size = os.fstat(f.fileno()).st_size
+
+        offset = 1
+        chars = 0
+        while offset <= size:
+            f.seek(-offset, os.SEEK_END)
+            b = ord(f.read(1))
+
+            if ignore_newlines:
+                if b == 0x0D or b == 0x0A:
+                    offset += 1
+                    continue
+
+            if b & 0b10000000 == 0 or b & 0b11000000 == 0b11000000:
+                # This is the first byte of a UTF8 character
+                chars += 1
+                if chars == count:
+                    # When `count` number of characters have been found, move current position back
+                    # with one byte (to include the byte just checked) and truncate the file
+                    f.seek(-1, os.SEEK_CUR)
+                    f.truncate()
+                    return
+            offset += 1
 
 fileman = Tk()
 fileman.wm_state('iconic')
@@ -54,43 +87,53 @@ else:
 
     round = int(0)
     for thing in range(properseq):
-        var = open(str(os.getcwd()) + str(dirslash) + str(int(thing + 1)) + str(dirslash) + 'var' + '.tmp', 'w')
+        extitems = list(file_path_list)
+        var = open(str(os.getcwd()) + str(dirslash) + str(int(thing + 1)) + str(dirslash) + 'var' + str(int(thing + 1)) + '.tmp', 'w')
         var.write(str('oneCharWidth = 10') + '\n' + str('oneCharHeight = 18') + '\n' + str('scaleFactor = 0.09'))
         var.close()
-        tmp = open(str(os.getcwd()) + str(dirslash) + str(int(thing + 1)) + str(dirslash) + 'tmp' + '.tmp', 'w')
+        tmp = open(str(os.getcwd()) + str(dirslash) + str(int(thing + 1)) + str(dirslash) + 'tmp' + str(int(thing + 1)) + '.tmp', 'w')
         tmp.write('[')
         tmp.write(str(file_path_list[int(thing)]))
+        del extitems[int(0)]
         if len(file_path_list) == int(properseq):
             tmp.write(']')
         else:
             tmp.write(', ')
-        try:int(int(len(file_path_list)) / int(properseq))
-        except:
-            modproc = math.floor(int(len(file_path_list)) / int(properseq))
-            extproc = int(len(file_path_list)) // int(properseq)
-            add2 = modproc
-            for hallo in range(int(modproc)):
-                tmp.write(str(list(file_path_list)[int(hallo + add2)]))
-                add2 = add2 + properseq
-            if round > modproc:
-                #finnish up round and modproc right here
-                round += int(1)
-                pass
-            del add2
-        else:
-            add = int(int(len(list(file_path_list))) / int(properseq))
-            add -= 1
-            add2 = properseq
-            for hello in range(add):
-                tmp.write(str(file_path_list[int(int(add2) + int(thing))]))
-                add2 = add2 + properseq
-                if int(hello + 1) != add:
-                    tmp.write(', ')
-                elif int(hello + 1) == add:
-                    tmp.write(']')
-            try:del hello
-            except:pass
-            del thing
-            del add
-            del add2
+            if isinstance(int(len(file_path_list)) / int(properseq), float):
+                evenproc = int(math.floor(int(int(len(list(file_path_list))) / int(properseq))))
+                extraproc = int(int(len(list(file_path_list))) // int(properseq))
+                add2 = properseq
+                for hello in range(int(evenproc - 1)):
+                    tmp.write(str(list(file_path_list)[int(int(add2) + int(thing))]))
+                    del extitems[int(int(add2) + int(thing))]
+                    add2 = add2 + properseq
+                    if int(hello + 1) != int(evenproc - 1):
+                        tmp.write(', ')
+                    elif int(hello + 1) == int(evenproc - 1):
+                        tmp.write(']')
+            if isinstance(int(len(file_path_list)) / int(properseq), int):
+                add = int(int(len(list(file_path_list))) / int(properseq))
+                add -= 1
+                add2 = properseq
+                for hello in range(add):
+                    tmp.write(str(file_path_list[int(int(add2) + int(thing))]))
+                    add2 = add2 + properseq
+                    if int(hello + 1) != add:
+                        tmp.write(', ')
+                    elif int(hello + 1) == add:
+                        tmp.write(']')
+                try:del hello
+                except:pass
+                del thing
+                del add
+                del add2
         tmp.close()
+    truncate_utf8_chars(os.getcwd() + str(dirslash) + '1' + str(dirslash) + 'tmp1.tmp', 1)
+    with open(os.getcwd() + str(dirslash) + '1' + str(dirslash) + 'tmp1.tmp', 'a') as tmpaft:
+        tmpaft.write(', ')
+        for items in range(int(len(extitems) - 1)):
+            #print(extitems)
+            tmpaft.write(str(list(extitems[int(items)])))
+            if items != int(range(int(len(extitems) -1))[int(-1)]):
+                tmpaft.write(', ')
+        tmpaft.write(']')
