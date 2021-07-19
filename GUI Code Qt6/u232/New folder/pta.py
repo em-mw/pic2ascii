b55687a71d7aa844fe43e3a3d3b1adaf6d008188
em-mw <01350311max247@gmail.com>
2021-07-19 15:24:40 -0500

    revamped code and ui (added costom charlist to gui)

--- GUI Code Qt6/u232/New folder/pta.py
+++ GUI Code Qt6/u232/New folder/pta.py
@@ -1,6 +1,3 @@
-import pic2ascii
-from PyQt6 import QtWidgets
-
 import math
 #from multiprocessing import Process
 from time import sleep
@@ -29,14 +26,6 @@ class start:
         return charArray[math.floor(inputInt*interval)]
     
     def main(self):
-        # Importing the bar
-        MainWindow = QtWidgets.QMainWindow()
-        ui = pic2ascii.Ui_MainWindow()
-        ui.setupUi(MainWindow)
-        bar = ui.progressBar
-        bar2 = ui.progressBar_2
-        ###
-        
         oneCharWidth = 10
         oneCharHeight = 18
         
@@ -122,8 +111,7 @@ class start:
                 outputImage.save(str(os.getcwd()) + str(dirslash) + 'outputPictureFiles' + str(dirslash) + 'output' + str(x) + '.png')
                 if int(len(file_path_list)) >= int(x + 1):
                     print(f'{fg.rs}\n\nImage {x} is done, going to next image\n\n')
-                bar.setValue(int(math.ceil(int(x) / len(file_path_list)) * 100))
-                bar2.setValue(int(math.ceil(int(x) / len(file_path_list)) * 100))
+                
 #for class
 p = start()
 
