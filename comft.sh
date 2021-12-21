e638c91c1169b961bf80b6fb0646241efb31bb06
em-mw <01350311max247@gmail.com>
2021-12-21 13:56:26 -0600

    added buggy-beta

--- comft.sh
+++
@@ -1,4 +0,0 @@
-git remote add github https://github.com/ErMax-Inc/pic2ascii.git
-git remote add gitlab https://gitlab.com/ermax-inc/pic2ascii.git
-git remote add all https://gitlab.com/ermax-inc/pic2ascii.git
-git remote set-url --add all https://github.com/ErMax-Inc/pic2ascii.git
