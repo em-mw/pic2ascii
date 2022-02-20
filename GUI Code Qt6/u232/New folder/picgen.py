d1a773eda6356997808b41f77d787ed1c13be22e
mw <01350311max247@gmail.com>
2022-02-19 22:42:31 -0600

    revamped the broken

--- GUI Code Qt6/u232/New folder/picgen.py
+++ GUI Code Qt6/u232/New folder/picgen.py
@@ -1,7 +1,7 @@
 import os, shutil, json
 
 def lols(file_path_list, processes):
-    print(os.path.isdir(str(os.getcwd()) + str(os.sep) + 'pic2asciitemp'))
+    #print(os.path.isdir(str(os.getcwd()) + str(os.sep) + 'pic2asciitemp'))
 
     if os.path.isdir(str(os.getcwd()) + str(os.sep) + 'pic2asciitemp'):
         shutil.rmtree(str(os.getcwd()) + str(os.sep) + 'pic2asciitemp')
@@ -11,7 +11,10 @@ def lols(file_path_list, processes):
             shutil.rmtree(str(os.getcwd()) + str(os.sep) + 'pic2asciitemp' + os.sep + str(int(folder + 1)))
         os.mkdir(str(os.getcwd()) + str(os.sep) + 'pic2asciitemp' + os.sep + str(int(folder + 1)))
     
-    if len(file_path_list) % 2 == 0:
+    print(list(file_path_list))
+    print(processes)
+    input(len(file_path_list) % processes)
+    if len(file_path_list) % processes == 0:
         files_per_json = int(len(file_path_list) / processes)
         file_iterval = files_per_json
         process_iteration = 1
@@ -30,8 +33,21 @@ def lols(file_path_list, processes):
 
     else:
         files_per_json = int(len(file_path_list) // processes)
+        file_interval = files_per_json
         ext_files = int(len(file_path_list) % processes)
         process_iteration = 1
         mk_json = []
-        for file in range(int(files_per_json * processes))
-#lols(['1', '2', '3', '4', '5', '6', '7', '8'], 8)
+        for file in range(int(files_per_json * processes)):
+            mk_json.append(file_path_list[file])
+            if file + 1 == file_interval:
+                with open(os.getcwd() + os.sep + 'pic2asciitemp' + os.sep + str(process_iteration) + os.sep + str(process_iteration) + '.json', 'w') as the_json_file:
+                    json.dump(mk_json, the_json_file)
+                process_iteration += 1
+                file_interval += files_per_json
+                mk_json = []
+        with open(os.getcwd() + os.sep + 'pic2asciitemp' + os.sep + str(processes) + os.sep + str(processes) + '.json', 'r') as the_json_file:
+            the_json_file.seek(0)
+            mk_json = json.load(the_json_file).copy()
+            mk_json += file_path_list[int(-1 * ext_files):]
+        with open(os.getcwd() + os.sep + 'pic2asciitemp' + os.sep + str(processes) + os.sep + str(processes) + '.json', 'w') as the_json_file:
+            json.dump(mk_json, the_json_file)
